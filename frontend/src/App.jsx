a5dfa0b37730aafbaba1fed6ab13adfd120085d2
init-chit <chitm538@gmail.com>
2026-09-11 00:20:02 +0630

    feat: add admin payment methods route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import TopUp from './pages/TopUp';
 import Gacha from './pages/Gacha';
 import Inventory from './pages/Inventory';
 import Admin from './pages/Admin';
+import AdminPaymentMethods from './pages/AdminPaymentMethods';
 import Profile from './pages/Profile';
 import GameDetail from './pages/GameDetail';
 import AllGames from './pages/AllGames';
@@ -26,6 +27,7 @@ function App() {
         <Route path="/gacha" element={<Gacha />} />
         <Route path="/inventory" element={<Inventory />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/payment-methods" element={<AdminPaymentMethods />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/games" element={<AllGames />} />
         <Route path="/games/:id" element={<GameDetail />} />
